725e0abef786ed0f32bdf85e26b0058b22680142
manuel.bit <emmanuel_njuguna@outlook.com>
2019-11-20 11:03:04 +0300

    add html form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,2 +1,9 @@
 from . import auth
 from flask import render_template
+from .forms import signUpForm
+
+@auth.route('/signup')
+def signup():
+  form = signUpForm()
+  
+  return render_template("signup.html")
